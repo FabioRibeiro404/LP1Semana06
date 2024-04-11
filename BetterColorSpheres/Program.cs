8cc11bdccb59639098a51a1f1447f90d6df1d968
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-04-11 11:21:04 +0100

    Added property manipulation in color

--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -6,7 +6,20 @@ namespace BetterColorSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Color c1 = new Color(100, 100, 100);
+            int currentColor1 = c1.Red;
+            int currentColor2 = c1.Green;
+            int currentColor3 = c1.Blue;
+
+            Color color1 = new Color(currentColor1, currentColor2, currentColor3);
+
+            Color c2 = new Color(100, 200, 200);
+            int currentColor4 = c2.Red;
+            int currentColor5 = c2.Green;
+            int currentColor6 = c2.Blue;
+
+            Sphere sphere = new Sphere();
+
         }
     }
 }
--- BetterColorSpheres/Sphere.cs
+++ BetterColorSpheres/Sphere.cs
@@ -7,6 +7,48 @@ namespace BetterColorSpheres
 {
     public class Sphere
     {
-        
+        private Color color;
+        private int radius;
+        private int numThrown = 0;
+
+        public Sphere(Color color, int radius)
+        {
+            this.color = color;
+            this.radius = radius;
+        }
+        public int Radius
+        {
+            get => radius;
+            set => radius = value;
+        }
+        public Color BallColor
+        {
+            get => color;
+            set => color = value;
+        }
+        public int NumberOfThrown
+        {
+            get => numThrown;
+            set => numThrown = value;
+        }
+
+        public int Thrown()
+        {
+            if(radius > 0)
+            {
+                numThrown++;
+            }
+            return numThrown;
+        }
+
+        public int Pop()
+        {
+            radius = 0;
+            return radius;
+        }
+        public int GetTimesThrown()
+        {
+            return numThrown;
+        }
     }
 }
